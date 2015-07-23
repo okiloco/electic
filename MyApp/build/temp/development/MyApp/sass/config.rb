a389af0f544e786d914e719f5bbb0ebafa7f5052
okiloco <okiloco2@gmail.com>
2015-07-22 22:58:44 -0500

    MyApp subida

---
+++ MyApp/build/temp/development/MyApp/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'C:\wamp\www\test\MyApp/build/.sass-cache'
---
+++ MyApp/build/temp/development/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\wamp\www\test\MyApp\app.js
