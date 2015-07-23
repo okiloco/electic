5bc6a640257d729d26d3a43378f4c4f363631aa6
okiloco <okiloco2@gmail.com>
2015-07-23 08:18:13 -0500

    CodeIgniter Include

--- MyApp/app.js
+++ MyApp/app.js
@@ -11,7 +11,6 @@ Ext.application({
     requires: [
         'MyApp.view.main.Main'
     ],
-
     // The name of the initial view to create. With the classic toolkit this class
     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
     // modern toolkit, the main view will be added to the Viewport.
--- MyApp/build/temp/production/MyApp/sass/config.rb
+++ MyApp/build/temp/production/MyApp/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'C:\wamp\www\electic\MyApp/build/.sass-cache'
+cache_path = 'C:\wamp\www\dashboard\MyApp/build/.sass-cache'
